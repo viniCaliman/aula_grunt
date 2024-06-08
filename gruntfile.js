ee23bc9d8da061cd26af9e79eaed345a9c309fbd
Vinicius Paiva Caliman <vinicaliman@gmail.com>
2024-06-08 12:18:58 -0300

    fourth lesson: iniciando o projeto do sorteador usando o less e separando o ambiente de dev e prod

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt){
         less: {
             development: {
                 files: {
-                    'main.css': 'main.less'
+                    'dev/styles/main.css': 'src/styles/main.less'
                 }
             },
             production: {
@@ -12,44 +12,14 @@ module.exports = function (grunt){
                     compress: true,
                 },
                 files: {
-                    'main.min.css': 'main.less',
+                    'dist/styles/main.min.css': 'src/styles/main.less',
                 }
             }
         },
-        sass: {
-            dist: {
-                options:{
-                    style: 'compressed',
-                },
-                files : {
-                    'main2.css': 'main.scss',
-                }
-            }
-        },
-        concurrent: {
-            target: ['olaGrunt','tarefaDemorada', 'less', 'sass']
-        },
     })
-
-    grunt.registerTask('olaGrunt', function() {
-        const done = this.async();
-        setTimeout(function() {
-            console.log("Olá grunt");
-            done();
-        }, 3000);
-    })
-
-    grunt.registerTask('tarefaDemorada', function() {
-        const done = this.async();
-        setTimeout(function() {
-            console.log("Olá grunt");
-            done();
-        }, 6000);
-    })
-
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-concurrent');
 
-    grunt.registerTask('default', ['concurrent'] )
+    grunt.registerTask('default', ['less:development'] )
+    grunt.registerTask('build', ['less:production'] )
+
 }

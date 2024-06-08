2a3d192955e2d81e41a5b6b2ca1e8781c430ddd4
Vinicius Paiva Caliman <vinicaliman@gmail.com>
2024-06-08 11:06:27 -0300

    second lesson: criando as tarefas padrão e tarefas assíncronas

--- gruntfile.js
+++ gruntfile.js
@@ -2,4 +2,14 @@ module.exports = function (grunt){
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
     })
+
+    grunt.registerTask('olaGrunt', function() {
+        const done = this.async();
+        setTimeout(function() {
+            console.log("Olá grunt");
+            done();
+        }, 3000);
+    })
+
+    grunt.registerTask('default', ['olaGrunt'])
 }
